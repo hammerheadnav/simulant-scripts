57f1be9fbf16ba48ff66105e382c0957158acc43
Brian Oberholtzer <brian@oberholtzer.net>
2021-06-28 17:06:56 -0400

    add 2-second speed cycle script.

---
+++ Scripts/BikeSpeedSensor/hh-2-second-speed-cycle.py
@@ -0,0 +1,29 @@
+﻿# This software is subject to the license described in the
+# LICENSE_A+SS.txt file included with this software distribution.
+# You may not use this file except in compliance with this license.
+#
+# Copyright (c) Garmin Canada Inc. 2018
+# All rights reserved.
+
+# This scripts sets up a bike cadence sensor to transmit the specified
+# range of sweeping values. Refer to the sesnsorSimulation module for additional
+# details on the sweeping mechanism
+from System.Timers import Timer
+
+eventTimer = Timer(1000) # convert time to milliseconds
+
+# SWEEP CONFIGURATION - Modify the values in the following four lines to change the sweep characteristics
+# Make sure that all of these are doubles (use a decimal point!) so that calculations are accurate
+
+simulator.Speed = 10
+
+def update(sender, args):
+   simulator.Speed = 30 if simulator.Speed == 10 else 10
+
+def stopScript():
+   eventTimer.Stop()
+   simulator.TurnOff()
+
+eventTimer.Elapsed += update
+simulator.TurnOn()
+eventTimer.Start()
