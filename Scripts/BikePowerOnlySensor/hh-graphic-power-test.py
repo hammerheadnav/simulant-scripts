fdef51753186200faa8f1b21740545e0566ed52e
Brian Oberholtzer <brian@oberholtzer.net>
2021-06-29 07:14:01 -0400

    added graphic power script

---
+++ Scripts/BikePowerOnlySensor/hh-graphic-power-test.py
@@ -0,0 +1,32 @@
+﻿# This software is subject to the license described in the
+# LICENSE_A+SS.txt file included with this software distribution.
+# You may not use this file except in compliance with this license.
+#
+# Copyright (c) Garmin Canada Inc. 2018
+# All rights reserved.
+
+# This scripts sets up a bike power sensor to transmit the specified
+# range of sweeping values. Refer to the sesnsorSimulation module for additional
+# details on the sweeping mechanism
+
+# from sensorSimulation import sweeper
+from System.Timers import Timer
+import System
+
+rventTimer = Timer(500) # convert time to milliseconds
+# sweepPower = sweeper()
+
+simulator.InstantaneousPower = 0 # sweepPower.minValue
+
+def update(sender, args):
+    simulator.InstantaneousPower = (simulator.InstantaneousPower + 50) % 550 
+
+def stopScript():
+#   sweepPower.stop()
+   eventTimer.Stop()
+   simulator.TurnOff()
+
+eventTimer.Elapsed += update
+simulator.TurnOn()
+eventTimer.Start()
+# sweepPower.start()
